c0d3d25625ef1adb1261395e8c39b476a165244b
Zegfried <metropoliten17@mail.ru>
2017-01-25 20:15:07 +0300

    + ::findAll()

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require __DIR__ .'/autoload.php';
+    require __DIR__ .'/autoload.php';
 
-$db = new \App\Db();
-//$res = $db->execute('CREATE TABLE foo (id SERIAL)');
-$data = $db->query('SELECT * FROM foo');
-var_dump($data);
+    $users = \App\Models\User::findAll();
+    var_dump($users);
+
+?>
