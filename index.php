a7b9acfdc99ffbd4ed92a68b4d9824b1d09dd214
Zegfried <metropoliten17@mail.ru>
2017-01-25 19:09:44 +0300

    + метод query() класса Db

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 require __DIR__ .'/autoload.php';
 
 $db = new \App\Db();
-$res = $db->execute('CREATE TABLE foo (id SERIAL)');
-var_dump($res);
+//$res = $db->execute('CREATE TABLE foo (id SERIAL)');
+$data = $db->query('SELECT * FROM foo');
+var_dump($data);
