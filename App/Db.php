704db72bdf7fb98b25278a7d0efeffbf4a7cf894
Zegfried <metropoliten17@mail.ru>
2017-01-25 18:52:32 +0300

    + метод execute() класса Db

--- App/Db.php
+++ App/Db.php
@@ -6,8 +6,16 @@ namespace App;
 
 class Db
 {
-  public function __construct()
-  {
-      echo 'Hellow, Db!';
-  }
+    protected $dbh;
+    public function __construct()
+    {
+      $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
+    }
 }
